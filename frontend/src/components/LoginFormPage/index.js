d5bf9bdf651722ed6df41fad77a3370c09dca266
Tadd JV <taddjv111@gmail.com>
2022-11-07 16:24:42 -0500

    si fix 2

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -20,8 +20,7 @@ function LoginFormPage() {
     return dispatch(sessionActions.login({ credential, password })).catch(
       async (res) => {
         const data = await res.json();
-        if (data && data.errors) setErrors(data.message);
-        console.log(errors);
+        if (data && data.message) setErrors([data.message]);
       }
     );
   };
